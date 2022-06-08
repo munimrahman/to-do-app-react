5577c45d2bb80d43ad9a7e9f05d4ee25e80bbfc6
munimrahman <munimrh@gmail.com>
2022-06-08 19:32:45 +0600

    first commit

---
+++ src/components/todos/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Todos extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="display-4 text-center mb-5">To Do App</h1>
+            </div>
+        );
+    }
+}
+
+export default Todos;
